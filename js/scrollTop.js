635d718ac7cdd96e67709f4d94f740c432c93e06
Kamil Błoński <kblonski02@gmail.com>
2024-10-05 22:05:10 +0200

    upload files

--- js/scrollTop.js
+++ js/scrollTop.js
@@ -2,7 +2,7 @@ const scrollToTopBtn = document.createElement('button');
 scrollToTopBtn.classList = 'scroll-btn'
 scrollToTopBtn.innerText = 'GO TO UP';
 const body = document.getElementsByTagName('body')[0].appendChild(scrollToTopBtn);
-scrollToTopBtn.addEventListener('click', ()=>{
+scrollToTopBtn.addEventListener('click', () => {
     window.scrollTo({
         top: 0,
         behavior: 'smooth'
@@ -11,16 +11,16 @@ scrollToTopBtn.addEventListener('click', ()=>{
 window.addEventListener('scroll', setProgressBar);
 
 
-function setProgressBar(){
+function setProgressBar() {
     const calcProcHeight = Math.round(
         (window.scrollY / (document.body.scrollHeight - window.innerHeight)) * 100
     )
 
-    if (window.scrollY < 200){
+    if (window.scrollY < 200) {
         scrollToTopBtn.style.display = 'none';
     } else {
         scrollToTopBtn.style.display = 'block';
-        
+
     }
 
     const color1 = 'rgb(255, 195, 45)';
