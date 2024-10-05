d222dfd1c109e79cdc5a114d69b78fc8c3ead99c
Kamil Błoński <kblonski02@gmail.com>
2024-10-05 21:47:20 +0200

    Clean code

--- js/socials.js
+++ js/socials.js
@@ -4,18 +4,11 @@ const socialsList = [
 for (const social of socialsList) {
     const li = document.createElement('li');
     const a = document.createElement('a')
-    const img = document.createElement('img');
-
 
     a.innerText = social.name;
     a.href = social.link;
     a.target = '_blank'
-    // img.src = ".\\images\\icons\\link.png";
-    // console.log(img.src)
-    // img.ariaHidden = true;
-    // img.classList.add('link-icon')
 
-    // li.appendChild(img);
     li.appendChild(a);
 
     document.getElementById('socials').appendChild(li)
