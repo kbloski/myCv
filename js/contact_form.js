6c05bdd820d428f593c331226a3b2b57b2ca7691
Kamil Błoński <kblonski02@gmail.com>
2024-11-10 20:01:49 +0100

    Fix bug

--- js/contact_form.js
+++ js/contact_form.js
@@ -1,7 +1,8 @@
 const sendInformationEl = document.getElementById("send-information");
-// sendInformationEl.style.display = 'none'
 
 const buttonSend = document.getElementById("contact-button");
 buttonSend.addEventListener('click', () => {
-    sendInformationEl.style.display = 'block'
+    // sendInformationEl.style.display = 'block'
+    sendInformationEl.innerText =
+        "Formularz powinien automatycznie przekierować do domyślnej aplikacji pocztowej po naciśnięciu przycisku „Wyślij”. Jeśli tak się nie stanie, oznacza to, że wiadomość nie została wysłana i należy się skontaktować drogą mailową lub telefoniczną.";
 })
