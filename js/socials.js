30e9b0f2593088a852f093a8f2421a7227156944
Kamil Błoński <kblonski02@gmail.com>
2024-11-16 17:20:58 +0100

    Add link to linkedin

--- js/socials.js
+++ js/socials.js
@@ -1,6 +1,9 @@
 const socialsList = [
-  { name: "GitHub", link: "https://github.com/kbloski" },
-  { name: "LinkedIn", link: "https://github.com/kbloski" },
+    { name: "GitHub", link: "https://github.com/kbloski" },
+    {
+        name: "LinkedIn",
+        link: "https://www.linkedin.com/in/kamil-b%C5%82o%C5%84ski-1958b4297/",
+    },
 ];
 for (const social of socialsList) {
   const li = document.createElement("li");
