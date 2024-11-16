5409c6d136b097663d2bc21f4cd523291c89230c
Kamil Błoński <kblonski02@gmail.com>
2024-11-16 16:42:02 +0100

    Update

--- js/socials.js
+++ js/socials.js
@@ -1,4 +1,7 @@
-const socialsList = [{ name: "GitHub", link: "https://github.com/kbloski" }];
+const socialsList = [
+  { name: "GitHub", link: "https://github.com/kbloski" },
+  { name: "LinkedIn", link: "https://github.com/kbloski" },
+];
 for (const social of socialsList) {
   const li = document.createElement("li");
   const a = document.createElement("a");
