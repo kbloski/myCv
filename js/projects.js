32f2d56d9f3665d9f0a37b6c90d8c9c465aeb5b0
Kamil Błoński <kblonski02@gmail.com>
2024-11-22 20:20:37 +0100

    Update

--- js/projects.js
+++ js/projects.js
@@ -1,24 +1,30 @@
 const projectsList = [
     {
-        name: "SpendWise - now only kod review",
+        name: "WeatherApp - in progress",
         description:
         "Monorepo Application: pełnostackowy projekt z backendem w Node.js, Express z autoryzacją napisany w Typescript i frontendem w Vue3 z użyciem vuex i vue-router napisany w javascript.",
         link: "https://github.com/kbloski/SpendWise",
     },
     {
-        name: "photographer-website - ONLY CODE REVIEW",
+        name: "SpendWise - code review",
+        description:
+        "Monorepo Application: pełnostackowy projekt z backendem w Node.js, Express z autoryzacją napisany w Typescript i frontendem w Vue3 z użyciem vuex i vue-router napisany w javascript.",
+        link: "https://github.com/kbloski/SpendWise",
+    },
+    {
+        name: "photographer-website - code review",
         description:
         "Monorepo Application: pełnostackowy projekt z backendem w Node.js i frontendem w Next.js, oba w TypeScript.",
         link: "https://github.com/kbloski/photographer-website/",
     },
     {
-        name: "user-role-managment",
+        name: "User role managment system in NextJs",
         description:
             "Prosta aplikacja wykorzystująca zarządzanie dostępem do stron za pomocą roli uzytkowników i priorytetów, technologie: HTML, CSS, TypeScript, NextJs, AppRouter",
         link: "https://usermanagment-role.netlify.app/",
     },
     {
-        name: "todo-task-list",
+        name: "Todo task list",
         description:
             "Prosta aplikacja zadań do wykonania z możliwością pobrania pliku z zadaniami, technologie: HTML, CSS, JS",
         link: "https://kbloski.github.io/todoList/",
