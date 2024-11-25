3a4b5ca6a5136e8f7a900bdf0e67817c0f25a238
Kamil Błoński <kblonski02@gmail.com>
2024-11-25 03:14:34 +0100

    update clauses

--- js/clauses.js
+++ js/clauses.js
@@ -1,6 +1,17 @@
 const clauses = [
   "Wyrażam zgodę na przetwarzanie moich danych osobowych dla potrzeb niezbędnych do realizacji procesu rekrutacji zgodnie z Rozporządzeniem Parlamentu Europejskiego i Rady (UE) 2016/679 z dnia 27 kwietnia 2016 r. w sprawie ochrony osób fizycznych w związku z przetwarzaniem danych osobowych i w sprawie swobodnego przepływu takich danych oraz uchylenia dyrektywy 95/46/WE (RODO).",
 ];
-for (const clause of clauses) {
-  document.getElementById("clauses").innerText = clause;
+
+function initClauses(){
+  const elementsUl = document.querySelectorAll(`[data-id='clauses']`)
+
+  for(const ulEl of elementsUl){
+    for(const clause of clauses){
+      const liEl = document.createElement('li');
+      liEl.innerHTML = clause
+      ulEl.appendChild( liEl )
+    }
+  }
 }
+
+initClauses()
--- js/form_contact.js
+++ js/form_contact.js
@@ -1,18 +1,18 @@
-import { email } from "./personal.js";
+// import { email } from "./personal.js";
 
-document
-  .getElementById("form-contact")
-  .addEventListener("submit", (event) => sendEmail(event));
+// document
+//   .getElementById("form-contact")
+//   .addEventListener("submit", (event) => sendEmail(event));
 
-const sendEmail = (event) => {
-  event.preventDefault();
+// const sendEmail = (event) => {
+//   event.preventDefault();
 
-  const subject = document.getElementById("form-subject").value;
-  const body = document.getElementById("form-body").value;
+//   const subject = document.getElementById("form-subject").value;
+//   const body = document.getElementById("form-body").value;
 
-  const link = `mailto:${email}?subject=${subject}&body=${body}`;
-  window.location.href = link;
-};
+//   const link = `mailto:${email}?subject=${subject}&body=${body}`;
+//   window.location.href = link;
+// };
 
 
 // const sendInformationEl = document.getElementById("send-information");
