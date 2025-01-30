6af4d303720dacfcb0e617e4d52a5a5b1f6d791f
Kamil Błoński <kblonski02@gmail.com>
2025-01-30 14:56:36 +0100

    UpdateCV

--- js/additional_information.js
+++ js/additional_information.js
@@ -11,8 +11,8 @@ function addInformationToList(){
 }
 
 const informationList = [
-    "W razie potrzeby jestem gotów podjąć bezpłatny staż na okres 1-3 miesięcy w celu zdobycia doświadczenia i rozwoju zawodowego.",
-    "Rozważam także możliwość zmiany miejsca zamieszkania, aby wykonywać pracę w trybie stacjonarnym.",
+    // "W razie potrzeby jestem gotów rozważyć bezpłatny staż na okres 1-3 miesięcy w celu zdobycia doświadczenia i rozwoju zawodowego.",
+    // "Rozważam także możliwość zmiany miejsca zamieszkania, aby wykonywać pracę w trybie stacjonarnym.",
     `Tutaj jest moja strona portfolio: 
     <a href='https://kbloski.github.io/myCv/'>
         https://kbloski.github.io/myCv/
