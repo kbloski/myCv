8587dca208d92b8ebac024604abb75513e676091
Kamil Błoński <kblonski02@gmail.com>
2025-02-19 20:16:00 +0100

    Update

--- js/additional_information.js
+++ js/additional_information.js
@@ -14,5 +14,5 @@ const informationList = [
     "W razie potrzeby jestem gotów rozważyć bezpłatny staż na okres 1-3 miesięcy w celu zdobycia doświadczenia i rozwoju zawodowego.",
     // "Rozważam także możliwość zmiany miejsca zamieszkania, aby wykonywać pracę w trybie stacjonarnym.",
 ];
-
+ 
 addInformationToList()
