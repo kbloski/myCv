c8af3411b24d519bb583d3994a24c6ef0edd9b61
Kamil Błoński <kblonski02@gmail.com>
2025-03-15 18:10:21 +0100

    Add Background

--- src/features/ui/WorkingInProgress.tsx
+++
@@ -1,9 +0,0 @@
-function WorkingInProgress() {
-    return (
-        <div>
-            <h1>"Apologies, I'm currently working on a page."</h1>
-        </div>
-    );
-}
-
-export default WorkingInProgress;
