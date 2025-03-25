1c35f697fc03226d34d614fc8e4b343a0db7c14e
Kamil Błoński <kblonski02@gmail.com>
2025-03-25 11:48:08 +0100

    Add default path for styles shared

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
@@ -6,4 +7,10 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
     base: "/myCv/",
     plugins: [react(), tailwindcss()],
+    resolve: {
+        alias: {
+            "@styles": path.resolve(__dirname, "./src/shared/styles"),
+        },
+        extensions: [".js", ".jsx", ".ts", ".tsx", ".css", ".scss"],
+    },
 });
