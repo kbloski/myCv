a86e0ff6442f9340ba0cbad067f450127018acff
Kamil Błoński <kblonski02@gmail.com>
2025-03-28 15:51:29 +0100

    Add font avesome to projetc

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,7 @@
 import "./index.css";
 
 import "primeicons/primeicons.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 import RouterAppProvider from "./router";
 import Providers from "./providers";
