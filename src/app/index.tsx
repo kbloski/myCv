66f955d11e41781d26fec7b27512baa596b75f54
Kamil Błoński <kblonski02@gmail.com>
2025-05-10 17:34:47 -0800

    Udate

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,7 +3,6 @@ import "./index.css";
 import "primeicons/primeicons.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "devicon/devicon.min.css";
-// document.head.innerHTML += '<link rel="stylesheet" href="devicon.min.css">';
 
 import {
     RouterAppProvider
